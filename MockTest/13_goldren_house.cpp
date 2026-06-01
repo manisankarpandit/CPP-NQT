14379fd5148bb1680391ce7767a3677d5b671eff
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-02 01:53:00 +0530

    add

---
+++ MockTest/13_goldren_house.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n ,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    int i = 0;
+    int sum = 0;
+    
+}
---
+++ String Based/3_Ascii_value.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    cout << "The ASCII value of " << ch << " is " << (int)(ch);
+}
