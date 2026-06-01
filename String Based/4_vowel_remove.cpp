1c9457f808013e904b9c31c1a35c8c357367a240
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-02 02:06:14 +0530

    add

--- String Based/4_vowel_remove.cpp
+++ String Based/4_vowel_remove.cpp
@@ -8,28 +8,37 @@ bool isVowels(char ch){
 int main(){
     string s;
     getline(cin,s);
-    if(s.front()=='"' && s.back()=='"'){
-        s = s.substr(1,s.size()-2);
-    }
-    stringstream ss(s);
-    string temp;
-    vector<string>v;
-    while(getline(ss,temp,' ')){
-        v.push_back(temp);
-    }
-
-    for(int i = 0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
     string t;
-    for(int i = 0;i<v.size();i++){
-        string str = v[i];
-        for(int j = 0;j<str.size();j++){
-            if(!isVowels(str[j])){
-                t+=str[j];
-            }
+    for(int j = 0;j<s.size();j++){
+        if(!isVowels(s[j])){
+            t+=s[j];
         }
     }
     cout<<t;
+
+    //user input
+    // if(s.front()=='"' && s.back()=='"'){
+    //     s = s.substr(1,s.size()-2);
+    // }
+    // stringstream ss(s);
+    // string temp;
+    // vector<string>v;
+    // while(getline(ss,temp,' ')){
+    //     v.push_back(temp);
+    // }
+
+    // for(int i = 0;i<v.size();i++){
+    //     cout<<v[i]<<" ";
+    // }
+    // cout<<endl;
+    // string t;
+    // for(int i = 0;i<v.size();i++){
+    //     string str = v[i];
+    //     for(int j = 0;j<str.size();j++){
+    //         if(!isVowels(str[j])){
+    //             t+=str[j];
+    //         }
+    //     }
+    // }
+    // cout<<t;
 }
